98c72ecc1396914b3c128c3fa5dc897832d4deb8
Taco Vader <taco.vader@crisp-e.com>
2023-09-26 21:37:12 +0200

    Start PoC Social login

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('https://my-custom-domain.com', 'public-anon-key')
+
+function signInWithGoogle() {
+  // Add login logic
+  alert(import.meta.env.MODE)
+}
+
+function signOut() {
+  // Add logout logic
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p><Button label="Log in" @click="signInWithGoogle" /></p>
+  <p><Button label="Log out" @click="signOut" /></p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './assets/main.css'
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+
+const app = createApp(App)
+app.use(PrimeVue)
+app.component('Button', Button)
+app.mount('#app')
