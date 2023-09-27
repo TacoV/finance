6afb654765bd9ad1fed79946a61b5daa46e6aa86
TacoV <TacoV@users.noreply.github.com>
2023-09-27 07:24:03 +0000

    Add .env.local generation logic tot startup script

--- spin-up-dev.sh
+++ spin-up-dev.sh
@@ -1,5 +1,11 @@
 git pull
 
+if [ ! -f .env.local ]; then
+    cp -n .env.local.example .env.local
+    echo 'Nieuwe .env.local klaargezet. Vul de gegevens aan en start dit script opnieuw!'
+    exit
+fi
+
 npm install
 npm run format
 npm run dev
