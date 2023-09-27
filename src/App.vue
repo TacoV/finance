a53cdce69c289a029e0a8aeb23ff5a5c721469e8
TacoV <TacoV@users.noreply.github.com>
2023-09-27 09:53:18 +0000

    Display uploaded files

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,14 @@ import { type Session } from '@supabase/gotrue-js/dist/main/lib/types'
 import FileUpload from 'primevue/fileupload'
 import { type FileUploadUploaderEvent } from 'primevue/fileupload'
 
+const bucketName = 'dumps'
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 )
+
 const userSession = ref<Session | null>(null)
+const fileList = ref<any[]>([])
 
 async function signInWithGoogle() {
   const { error } = await supabase.auth.signInWithOAuth({
@@ -29,24 +32,44 @@ async function signOut() {
 
 supabase.auth.onAuthStateChange((event, session) => {
   userSession.value = session
+  if (userSession.value?.user.id) {
+    retrieveFiles()
+  }
 })
 
 async function uploadFile(uploadEvent: FileUploadUploaderEvent) {
+  const folder = userSession.value?.user.id
   for (const bestand of uploadEvent.files as File[]) {
-    const { error } = await supabase.storage.from('dumps').upload(bestand.name, bestand)
+    const { error } = await supabase.storage
+      .from(bucketName)
+      .upload(folder + '/' + bestand.name, bestand)
     if (error) {
       alert('Error uploading: ' + error.message)
     }
+  }
+  await retrieveFiles()
+}
+
+async function retrieveFiles() {
+  const { data, error } = await supabase.storage.from(bucketName).list(userSession.value?.user.id)
+  if (error) {
+    alert('Error listing files: ' + error.message)
+  }
+  if (data) {
+    fileList.value = data
   }
 }
 </script>
 
 <template>
-  <p v-if="userSession === null">
+  <div v-if="userSession === null">
     <Button label="Log in" @click="signInWithGoogle" />
-  </p>
-  <p v-else>
+  </div>
+  <div v-else>
+    <ul>
+      <li v-for="file in fileList">{{ file.name }}</li>
+    </ul>
     <FileUpload mode="basic" name="csvfiles[]" customUpload @uploader="uploadFile" :auto="true" />
     <Button :label="'Log out ' + userSession?.user.user_metadata.full_name" @click="signOut" />
-  </p>
+  </div>
 </template>
