11212baf66e80a250fb14a630476641645ee84cc
TacoV <TacoV@users.noreply.github.com>
2023-09-27 08:00:20 +0000

    Complete basic Google login workflow

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <script setup lang="ts">
 import { createClient } from '@supabase/supabase-js'
+import { ref } from 'vue'
+import { type Session } from '@supabase/gotrue-js/dist/main/lib/types'
 
-const supabase = createClient('https://my-custom-domain.com', 'public-anon-key')
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
+const userSession = ref<Session | null>(null)
 
-function signInWithGoogle() {
-  // Add login logic
-  alert(import.meta.env.MODE)
+async function signInWithGoogle() {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'google'
+  })
+  if (error) {
+    alert('Error logging in: ' + error.message)
+  }
 }
 
-function signOut() {
-  // Add logout logic
+async function signOut() {
+  const { error } = await supabase.auth.signOut()
+  if (error) {
+    alert('Error logging out: ' + error.message)
+  }
 }
+
+supabase.auth.onAuthStateChange((event, session) => {
+  userSession.value = session
+})
 </script>
 
 <template>
-  <p><Button label="Log in" @click="signInWithGoogle" /></p>
-  <p><Button label="Log out" @click="signOut" /></p>
+  <p v-if="userSession === null">
+    <Button label="Log in" @click="signInWithGoogle" />
+  </p>
+  <p v-else>
+    <Button :label="'Log out ' + userSession?.user.user_metadata.full_name" @click="signOut" />
+  </p>
 </template>
