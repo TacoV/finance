987433b74ebdadcee41cb8d58c5bfd76e6531ae8
TacoV <TacoV@users.noreply.github.com>
2023-09-28 11:11:37 +0000

    Fix the CORS issue

--- supabase/functions/process-files/index.ts
+++ supabase/functions/process-files/index.ts
@@ -6,7 +6,7 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
 
 serve(async (req) => {
